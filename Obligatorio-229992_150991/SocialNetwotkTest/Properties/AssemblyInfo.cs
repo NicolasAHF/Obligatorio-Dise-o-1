8b8a9c64124f4a268f1bf3a69c55ec7a791b0b17
NicolasAHF <nicolashernandezf22@gmail.com>
2021-09-13 23:34:30 -0300

    Agrego proyectos

---
+++ Obligatorio-229992_150991/SocialNetwotkTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SocialNetwotkTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SocialNetwotkTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a4b79bc-0173-4775-aff0-5597cf380404")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Obligatorio-229992_150991/SocialNetwotkTest/UserTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SocialNetwork;
+
+namespace SocialNetwotkTest
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreateUserWithValidName()
+        {
+            User user = new User();
+            user.Username = "User1";
+            string validUsername = "User1";
+            Assert.AreEqual(user.Username, validUsername);
+        }
+    }
+}
