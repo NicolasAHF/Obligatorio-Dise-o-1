6852ea9dc49de1b4e197816a6279c6e74c6e30e1
NicolasAHF <nicolashernandezf22@gmail.com>
2021-09-20 23:40:35 -0300

    Creo clase ListeningNow con sus atributos y metodos y sus Test

---
+++ Obligatorio-229992_150991/SocialNetwork/ListeningNow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork
+{
+    public class ListeningNow
+    {
+        private string _songName;
+        private string _artist;
+        private string _albumName;
+
+
+        public string SongName 
+        {
+            get { return _songName; }
+            private set => SetSongName(value);
+        }
+
+        public string Artist 
+        {
+            get { return _artist; }
+            private set => SetArtist(value);
+        }
+
+        public string AlbumName
+        {
+            get { return _albumName; }
+            private set => SetAlbumName(value);
+        }
+
+        public ListeningNow(string songName, string artist, string albumName)
+        {
+            this.SongName = songName;
+            this.Artist = artist;
+            this.AlbumName = albumName;
+        }
+
+        public void SetSongName(string songName)
+        {
+            if (EmptyString(songName))
+            {
+                throw new InvalidOperationException("Nombre de la cancion no puede ser vacio");
+            }
+            else
+            {
+                this._songName = songName;
+            }
+        }
+
+        public void SetArtist(string artist)
+        {
+            if (EmptyString(artist))
+            {
+                throw new InvalidOperationException("El artista no puede ser vacio");
+            }
+            else
+            {
+                this._artist = artist;
+            }
+        }
+
+        public void SetAlbumName(string albumName)
+        {
+            if (EmptyString(albumName))
+            {
+                throw new InvalidOperationException("El nombre del album no puede ser vacio");
+            }
+            else
+            {
+                this._albumName = albumName;
+            }
+        }
+
+        private bool EmptyString(string value)
+        {
+            return value.Length.Equals(0);
+        }
+
+    }
+}
---
+++ Obligatorio-229992_150991/SocialNetwotkTest/ListeningNowTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SocialNetwork;
+
+namespace SocialNetwotkTest
+{
+    [TestClass]
+    public class ListeningNowTest
+    {
+        ListeningNow validListeningNow;
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+            validListeningNow = new ListeningNow("High", "Maria Becerra", "Animal");
+
+        }
+
+        [TestMethod]
+        public void ValidSongNameCreation()
+        {
+            string validSongName = "High";
+            Assert.AreEqual(validListeningNow.SongName, validSongName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidSongNameCreation()
+        {
+            ListeningNow invalidListeningNow = new ListeningNow("", "Maria Becerra", "Animal");
+        }
+
+        [TestMethod]
+        public void ValidArtistCreation()
+        {
+            string validArtist = "Maria Becerra";
+            Assert.AreEqual(validListeningNow.Artist, validArtist);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidArtistCreation()
+        {
+            ListeningNow invalidListeningNow = new ListeningNow("High", "", "Animal");
+        }
+
+        [TestMethod]
+        public void ValidAlbumNameCreation()
+        {
+            string validAlbumName = "Animal";
+            Assert.AreEqual(validListeningNow.AlbumName, validAlbumName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void InvalidAlbumNameCreation()
+        {
+            ListeningNow invalidListeningNow = new ListeningNow("High", "Maria Becerra", "");
+        }
+    }
+}
