7e4f2373395bb745327828a3cbcd9cb82faded15
NicolasAHF <nicolashernandezf22@gmail.com>
2021-09-24 21:59:26 -0300

    Modifico login form y lo hago userControl

--- Obligatorio-229992_150991/UISocialNetwork/Login.cs
+++ Obligatorio-229992_150991/UISocialNetwork/Login.cs
@@ -7,22 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using SocialNetwork;
 
 namespace UISocialNetwork
 {
-    public partial class Login : Form
+    public partial class Login : UserControl
     {
-        private DirectoryUser users = new DirectoryUser();
         public Login()
         {
             InitializeComponent();
-        }
-
-        private void CreateUserBtn_Click(object sender, EventArgs e)
-        {
-            CreateUser formUser = new CreateUser(users);
-            formUser.Show();
         }
 
     }
