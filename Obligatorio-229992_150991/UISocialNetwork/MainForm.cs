6d8f52564e3ae6df2c18ad555ed090b1ce1602af
NicolasAHF <nicolashernandezf22@gmail.com>
2021-09-24 21:57:21 -0300

    Agrego formulario principal

---
+++ Obligatorio-229992_150991/UISocialNetwork/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UISocialNetwork
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            mainPanel.Controls.Clear();
+            UserControl login = new Login();
+            mainPanel.Controls.Add(login);
+        }
+
+        private void createUserBtn_Click(object sender, EventArgs e)
+        {
+            mainPanel.Controls.Clear();
+            UserControl createUser = new CreateUser();
+            mainPanel.Controls.Add(createUser);
+        }
+    }
+}
