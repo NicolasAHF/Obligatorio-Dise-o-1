a6c1cd95e8c339555844b004929b4a7d17c1696b
Nicolas Hernandez <NH229992@fi365.ort.edu.uy>
2021-09-26 15:49:00 -0300

    Creo perfil

---
+++ Obligatorio-229992_150991/UISocialNetwork/Profile.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace UISocialNetwork
+{
+    partial class Profile
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.avatar = new System.Windows.Forms.PictureBox();
+            this.usernameLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.avatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // avatar
+            // 
+            this.avatar.Location = new System.Drawing.Point(22, 18);
+            this.avatar.Name = "avatar";
+            this.avatar.Size = new System.Drawing.Size(145, 138);
+            this.avatar.TabIndex = 0;
+            this.avatar.TabStop = false;
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.usernameLbl.Location = new System.Drawing.Point(174, 31);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(0, 25);
+            this.usernameLbl.TabIndex = 1;
+            // 
+            // Profile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.usernameLbl);
+            this.Controls.Add(this.avatar);
+            this.Name = "Profile";
+            this.Size = new System.Drawing.Size(825, 488);
+            this.Load += new System.EventHandler(this.Profile_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.avatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox avatar;
+        private System.Windows.Forms.Label usernameLbl;
+    }
+}
---
+++ Obligatorio-229992_150991/UISocialNetwork/Profile.cs
@@ -0,0 +1,29 @@
+﻿using SocialNetwork;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UISocialNetwork
+{
+    public partial class Profile : UserControl
+    {
+        private User user;
+        public Profile(User user)
+        {
+            InitializeComponent();
+            this.user = user;
+        }
+
+        private void Profile_Load(object sender, EventArgs e)
+        {
+            usernameLbl.Text = user.Username;
+            avatar.ImageLocation = user.Avatar;
+        }
+    }
+}
