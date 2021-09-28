df5a1f995c2b373bff6f93e62dd7ae8d3fde0b60
NicolasAHF <nicolashernandezf22@gmail.com>
2021-09-27 22:27:17 -0300

    Merge mainwindow develop

---
+++ Obligatorio-229992_150991/UISocialNetwork/MainForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace UISocialNetwork
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.header = new System.Windows.Forms.Panel();
+            this.createUserBtn = new System.Windows.Forms.Button();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.marketplaceBtn = new System.Windows.Forms.Button();
+            this.profileBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LogoutBtn = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.header.SuspendLayout();
+            this.panelMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoScroll = true;
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Location = new System.Drawing.Point(165, 80);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(634, 390);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(74, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 126);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Obligatorio Diseño \r\nde aplicaciones 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // header
+            // 
+            this.header.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.header.Controls.Add(this.LogoutBtn);
+            this.header.Controls.Add(this.createUserBtn);
+            this.header.Controls.Add(this.loginBtn);
+            this.header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header.Location = new System.Drawing.Point(165, 0);
+            this.header.Margin = new System.Windows.Forms.Padding(2);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(634, 81);
+            this.header.TabIndex = 4;
+            // 
+            // createUserBtn
+            // 
+            this.createUserBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.createUserBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createUserBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.createUserBtn.Location = new System.Drawing.Point(450, 0);
+            this.createUserBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.createUserBtn.Name = "createUserBtn";
+            this.createUserBtn.Size = new System.Drawing.Size(92, 81);
+            this.createUserBtn.TabIndex = 3;
+            this.createUserBtn.Text = "Crear Usuario";
+            this.createUserBtn.UseVisualStyleBackColor = true;
+            this.createUserBtn.Click += new System.EventHandler(this.createUserBtn_Click);
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.loginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loginBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.loginBtn.Location = new System.Drawing.Point(542, 0);
+            this.loginBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(92, 81);
+            this.loginBtn.TabIndex = 2;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panelMenu.Controls.Add(this.marketplaceBtn);
+            this.panelMenu.Controls.Add(this.profileBtn);
+            this.panelMenu.Controls.Add(this.pictureBox1);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(2);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(165, 467);
+            this.panelMenu.TabIndex = 3;
+            // 
+            // marketplaceBtn
+            // 
+            this.marketplaceBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.marketplaceBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.marketplaceBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.marketplaceBtn.Location = new System.Drawing.Point(0, 130);
+            this.marketplaceBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.marketplaceBtn.Name = "marketplaceBtn";
+            this.marketplaceBtn.Size = new System.Drawing.Size(165, 49);
+            this.marketplaceBtn.TabIndex = 2;
+            this.marketplaceBtn.Text = "MarketPlace";
+            this.marketplaceBtn.UseVisualStyleBackColor = true;
+            this.marketplaceBtn.Visible = false;
+            // 
+            // profileBtn
+            // 
+            this.profileBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.profileBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.profileBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.profileBtn.Location = new System.Drawing.Point(0, 81);
+            this.profileBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.profileBtn.Name = "profileBtn";
+            this.profileBtn.Size = new System.Drawing.Size(165, 49);
+            this.profileBtn.TabIndex = 1;
+            this.profileBtn.Text = "Perfil";
+            this.profileBtn.UseVisualStyleBackColor = true;
+            this.profileBtn.Visible = false;
+            this.profileBtn.Click += new System.EventHandler(this.profileBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 81);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LogoutBtn
+            // 
+            this.LogoutBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.LogoutBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogoutBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LogoutBtn.Location = new System.Drawing.Point(358, 0);
+            this.LogoutBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.LogoutBtn.Name = "LogoutBtn";
+            this.LogoutBtn.Size = new System.Drawing.Size(92, 81);
+            this.LogoutBtn.TabIndex = 4;
+            this.LogoutBtn.Text = "Logout";
+            this.LogoutBtn.UseVisualStyleBackColor = true;
+            this.LogoutBtn.Visible = false;
+            this.LogoutBtn.Click += new System.EventHandler(this.LogoutBtn_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 467);
+            this.Controls.Add(this.header);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.mainPanel);
+            this.MaximumSize = new System.Drawing.Size(815, 506);
+            this.MinimumSize = new System.Drawing.Size(815, 506);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.header.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Panel header;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button createUserBtn;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button profileBtn;
+        private System.Windows.Forms.Button marketplaceBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LogoutBtn;
+    }
+}
