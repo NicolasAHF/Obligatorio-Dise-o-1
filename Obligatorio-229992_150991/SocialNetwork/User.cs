3bbda29c00bc2ba6338aa2a0a7a45a4b4768d3b7
ferxr3 <ferxr3@gmail.com>
2021-10-05 23:26:05 -0300

    Se agrega las feature de Album y de User

--- Obligatorio-229992_150991/SocialNetwork/User.cs
+++ Obligatorio-229992_150991/SocialNetwork/User.cs
@@ -18,8 +18,8 @@ namespace SocialNetwork
         private Direction _direction;
         private string _avatar;
         private bool _admin = false;
-        private List<User> _listfollowing = new List<User>();
-        private List<Album> _ListAlbums = new List<Album>();
+        private List<User> _followingList = new List<User>();
+        private List<Album> _albumList = new List<Album>();
 
         const int MIN_LENGTH_FOR_VALID_NAME = 5;
 
@@ -69,26 +69,26 @@ namespace SocialNetwork
             set => SetAdmin(value);
         }
 
-        public List<User> ListFollowing
+        public List<User> Following
         {
-            get { return _listfollowing; }
-            set { _listfollowing = value; }
+            get { return _followingList; }
+            set { _followingList = value; }
         }
 
-        public List<User> ListAlbum
+        public List<Album> AlbumList
         {
-            get { return _listAlbum; }
-            set { _listAlbum = value; }
+            get { return _albumList; }
+            set { _albumList = value; }
         }
 
-        public User(string username, string name, string lastname, DateTime birthday, Direction direction, string password, string avatar)
+        public User(string username, string passwordClear, string name, string lastname, DateTime birthday, Direction direction, string avatar)
         {
             this.Username = username;
             this.Name = name;
             this.Lastname = lastname;
             this.DateOfBirth = birthday;
             this.Direction = direction;
-            this._password = new Password(password);
+            this._password = new Password(passwordClear);
             this.Avatar = avatar;
             
         }
