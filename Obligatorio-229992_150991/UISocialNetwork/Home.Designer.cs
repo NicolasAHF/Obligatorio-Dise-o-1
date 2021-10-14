277c6748e78becc93e1403062b0ceadd26d127aa
NicolasAHF <nicolashernandezf22@gmail.com>
2021-10-13 21:12:13 -0300

    Se agrega boton status

--- Obligatorio-229992_150991/UISocialNetwork/Home.Designer.cs
+++ Obligatorio-229992_150991/UISocialNetwork/Home.Designer.cs
@@ -36,8 +36,9 @@ namespace UISocialNetwork
             this.listeningBtn = new System.Windows.Forms.Button();
             this.panelContent = new System.Windows.Forms.Panel();
             this.panelButtons = new System.Windows.Forms.Panel();
-            this.showContentPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.statusBtn = new System.Windows.Forms.Button();
             this.createAlbumBtn = new System.Windows.Forms.Button();
+            this.showContentPanel = new System.Windows.Forms.FlowLayoutPanel();
             this.panelButtons.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,21 +60,25 @@ namespace UISocialNetwork
             this.userList.TabIndex = 1;
             this.userList.Visible = false;
             this.userList.DoubleClick += new System.EventHandler(this.userList_DoubleClick);
+            this.userList.Leave += new System.EventHandler(this.userList_Leave);
             // 
             // searchBtn
             // 
+            this.searchBtn.BackColor = System.Drawing.Color.Maroon;
+            this.searchBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.searchBtn.Location = new System.Drawing.Point(262, 4);
             this.searchBtn.Name = "searchBtn";
-            this.searchBtn.Size = new System.Drawing.Size(124, 20);
+            this.searchBtn.Size = new System.Drawing.Size(127, 34);
             this.searchBtn.TabIndex = 2;
             this.searchBtn.Text = "Buscar";
-            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.UseVisualStyleBackColor = false;
             this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
             // 
             // lblErrorMsg
             // 
             this.lblErrorMsg.AutoSize = true;
-            this.lblErrorMsg.Location = new System.Drawing.Point(262, 31);
+            this.lblErrorMsg.Location = new System.Drawing.Point(259, 41);
             this.lblErrorMsg.Name = "lblErrorMsg";
             this.lblErrorMsg.Size = new System.Drawing.Size(29, 13);
             this.lblErrorMsg.TabIndex = 3;
@@ -82,12 +87,15 @@ namespace UISocialNetwork
             // 
             // listeningBtn
             // 
+            this.listeningBtn.BackColor = System.Drawing.Color.Maroon;
+            this.listeningBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.listeningBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.listeningBtn.Location = new System.Drawing.Point(429, 4);
             this.listeningBtn.Name = "listeningBtn";
-            this.listeningBtn.Size = new System.Drawing.Size(132, 20);
+            this.listeningBtn.Size = new System.Drawing.Size(132, 34);
             this.listeningBtn.TabIndex = 4;
             this.listeningBtn.Text = "Que estas escuchando";
-            this.listeningBtn.UseVisualStyleBackColor = true;
+            this.listeningBtn.UseVisualStyleBackColor = false;
             this.listeningBtn.Click += new System.EventHandler(this.listeningBtn_Click);
             // 
             // panelContent
@@ -101,12 +109,40 @@ namespace UISocialNetwork
             // 
             // panelButtons
             // 
+            this.panelButtons.Controls.Add(this.statusBtn);
+            this.panelButtons.Controls.Add(this.lblErrorMsg);
             this.panelButtons.Controls.Add(this.createAlbumBtn);
             this.panelButtons.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelButtons.Location = new System.Drawing.Point(0, 0);
             this.panelButtons.Name = "panelButtons";
             this.panelButtons.Size = new System.Drawing.Size(716, 86);
             this.panelButtons.TabIndex = 6;
+            // 
+            // statusBtn
+            // 
+            this.statusBtn.BackColor = System.Drawing.Color.Maroon;
+            this.statusBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.statusBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.statusBtn.Location = new System.Drawing.Point(429, 44);
+            this.statusBtn.Name = "statusBtn";
+            this.statusBtn.Size = new System.Drawing.Size(132, 30);
+            this.statusBtn.TabIndex = 9;
+            this.statusBtn.Text = "Crear frase de status";
+            this.statusBtn.UseVisualStyleBackColor = false;
+            this.statusBtn.Click += new System.EventHandler(this.statusBtn_Click);
+            // 
+            // createAlbumBtn
+            // 
+            this.createAlbumBtn.BackColor = System.Drawing.Color.Maroon;
+            this.createAlbumBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.createAlbumBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.createAlbumBtn.Location = new System.Drawing.Point(567, 4);
+            this.createAlbumBtn.Name = "createAlbumBtn";
+            this.createAlbumBtn.Size = new System.Drawing.Size(134, 34);
+            this.createAlbumBtn.TabIndex = 8;
+            this.createAlbumBtn.Text = "Crear Album";
+            this.createAlbumBtn.UseVisualStyleBackColor = false;
+            this.createAlbumBtn.Click += new System.EventHandler(this.createAlbumBtn_Click);
             // 
             // showContentPanel
             // 
@@ -117,23 +153,12 @@ namespace UISocialNetwork
             this.showContentPanel.Size = new System.Drawing.Size(709, 131);
             this.showContentPanel.TabIndex = 7;
             // 
-            // createAlbumBtn
-            // 
-            this.createAlbumBtn.Location = new System.Drawing.Point(567, 4);
-            this.createAlbumBtn.Name = "createAlbumBtn";
-            this.createAlbumBtn.Size = new System.Drawing.Size(132, 20);
-            this.createAlbumBtn.TabIndex = 8;
-            this.createAlbumBtn.Text = "Crear Album";
-            this.createAlbumBtn.UseVisualStyleBackColor = true;
-            this.createAlbumBtn.Click += new System.EventHandler(this.createAlbumBtn_Click);
-            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.Controls.Add(this.listeningBtn);
-            this.Controls.Add(this.lblErrorMsg);
             this.Controls.Add(this.searchBtn);
             this.Controls.Add(this.userList);
             this.Controls.Add(this.searchBox);
@@ -144,6 +169,7 @@ namespace UISocialNetwork
             this.Name = "Home";
             this.Size = new System.Drawing.Size(716, 480);
             this.panelButtons.ResumeLayout(false);
+            this.panelButtons.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,5 +186,6 @@ namespace UISocialNetwork
         private System.Windows.Forms.Panel panelButtons;
         private System.Windows.Forms.FlowLayoutPanel showContentPanel;
         private System.Windows.Forms.Button createAlbumBtn;
+        private System.Windows.Forms.Button statusBtn;
     }
 }
